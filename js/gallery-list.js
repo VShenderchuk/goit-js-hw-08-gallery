5f004ec72c41e31f7d864dc4b4242e95ceb3f0c5
VShenderchuk <64258673+VShenderchuk@users.noreply.github.com>
2020-11-04 20:12:50 +0200

    adds changes to js

--- js/gallery-list.js
+++ js/gallery-list.js
@@ -1,6 +1,6 @@
 import imagesArr from "./gallery-items.js";
 function createGalleryImages(image) {
-return image
+  return image
     .map(({ preview, original, description }) => {
       let i = `<li class="gallery__item">
   <a class="gallery__link" href="${original}">
@@ -22,23 +22,24 @@ const galleryContainer = document.querySelector(".gallery");
 
 galleryContainer.insertAdjacentHTML("beforeend", Markup);
 
-
 const lightbox = document.querySelector(`.js-lightbox`);
 const lightboxImage = document.querySelector(`.lightbox__image`);
 galleryContainer.addEventListener(`click`, onImageclick);
 function onImageclick(event) {
-    event.preventDefault();
-    if(event.target.nodeName !== `IMG`) {
-        return;
-    }
-    lightbox.classList.add(`is-open`);
-    lightboxImage.src = event.target.dataset.source;
-    lightboxImage.alt = event.target.alt;
-    document.body.style.overflow = "hidden";
+  event.preventDefault();
+  if (event.target.nodeName !== `IMG`) {
+    return;
+  }
+  lightbox.classList.add(`is-open`);
+  lightboxImage.src = event.target.dataset.source;
+  lightboxImage.alt = event.target.alt;
+  document.body.style.overflow = "hidden";
 }
 
-const closeModalBtn = document.querySelector(`[data-action="close-lightbox]`);
-closeModalBtn.addEventListener(`click`)
-function onCloseModal (event) {
-    document.classList.remove(`is-open`);
+const closeModalBtn = document.querySelector(`.lightbox__button`);
+closeModalBtn.addEventListener(`click`, onCloseModal);
+function onCloseModal(event) {
+  lightbox.classList.remove(`is-open`);
+  lightboxImage.src = "";
+  lightboxImage.alt = "";
 }
